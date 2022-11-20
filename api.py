136ef3320c33429623b0110e445be7bacb9a4583
Eivar Morales <eivar.m@cheil.com>
2022-11-20 14:25:39 -0500

    added cors

--- api.py
+++ api.py
@@ -2,12 +2,20 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Union
 from engine import lexic
+from fastapi.middleware.cors import CORSMiddleware
+
 class Sentence(BaseModel):
     sentence: str
 
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
