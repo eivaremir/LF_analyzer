9aabd050ff8d5eb9bdcf98edd1f7b36d4f9f513f
eivar <eivaremir@gmail.com>
2022-11-21 13:06:55 -0500

    added more changes

--- lexic_tokens.py
+++ lexic_tokens.py
@@ -8,7 +8,7 @@ TOKENS ={
         "PARTICIPLE_VERB":["bought","Dyed","paid out","taken","been"],
         "NOUN":["Milk","Margarine","Cheese","Egg","Meat","Chicken","Rice","Beans","Noodles","Sugar","Onion","Tomatoes","Beer","Wine","money","cents"],
         "ADV":["morning","Now","After","Soon","Yet","Here","There","Much","Okay","Hardly","Bad","Almost","Usually","Never"],
-        "ARTCILE":["The","A","Some"],
+        "ARTICLE":["The","A","Some"],
         "PRONOUN":["I","You","He","She","Us","You","they","it","we"],
         "ADJECTIVE":["great","pretty","small","afternoon","important","animated","large","narrow","bass","length","Okay","cold","warm","easy","bad","tall","strong","same","new"]
 
@@ -17,11 +17,11 @@ TOKENS ={
     "SPANISH": {
         "PREPOSITION":["a","de","con","en","sin"],
         "CONJUNCTION":["pero","sino","y","o","porque","pues"],
-        "VERB":["comprando","compramos","teniendo","pagando","tomando","comprar","tener","pagar","tomar","comprado","tenido","pagado","tomado","soy","eres","es","somos","son","estoy"",","estás","está","estamos","están"],
-        "NOUN":["frijoles","leche","margarina","queso","huevo","carne","pollo","arroz","frijol","fideos","azúcar","cebolla","tomate","cerveza","vino"],
+        "VERB":["comprando","compramos","compraron","teniendo","pagando","tomando","comprar","tener","pagar","tomar","comprado","tenido","pagado","tomado","soy","eres","es","somos","son","estoy"",","estás","está","estamos","están"],
+        "NOUN":["frijoles","leche","margarina","queso","huevo","carne","pollo","arroz","frijol","fideos","azúcar","cebolla","tomate","cerveza","cervezas","vino"],
         "ADJECTIVE":["estupendo","bonita","pequeña","tarde","importante","animado","amplio","angosto","bajo","largo","bueno","frío","cálido","fácil","malo","alto","fuerte","mismo","nuevo","buen","mal","buena"],
         "ADV":["algo","mañana","ahora","después","pronto","todavía","aquí","ahí","mucho","bueno","buen","mal","difícilmente","malo","casi","generalmente"],
-        "ARTCILE":["el","la","los","las","un","una","uno","unos","unas"],
+        "ARTICLE":["el","la","los","las","un","una","uno","unos","unas"],
         "PRONOUN":["yo","tú","usted","él","ella","nosotros","nosotras","ustedes","ellos","ellas"],
         
     },
@@ -32,9 +32,9 @@ TOKENS ={
         "VERB":["comprando","tendo","pagando","pegando","comprar","ter","pagar","pegar","comprado","tido","pago","pegado","sou","é","somos","são","estou","está","estamos","estão"],
         "NOUN":["leite","margarina","queijo","ovo","carne","frango","arroz","feijões","macarrão","açúcar","cebola","tomates","cerveja","veio"],
         "ADV":["manhã","agora","depois","brevemente","ainda","aqui","lá","muito","ok","dificilmente","mau","quase","usualmente","nunca"],
-        "ARTCILE":["o","a","os","as","um","uma","um","uns","umas"],
+        "ARTICLE":["o","a","os","as","um","uma","um","uns","umas"],
         "PRONOUN":["eu","você","ele","ela","nós","vocês","eles","elas"],
-        "ADJECTIVE":["excelente","bonito","pequeña","tarde","importante","animado","ampla","estreito","Baixo","comprimento","Bom","frio","caloroso","fácil","mau","alta","Forte","mesmo","novo"],
+        "ADJECTIVE":["excelente","bonito","pequeña","tarde","importantes","importante","animado","ampla","estreito","Baixo","comprimento","Bom","frio","caloroso","fácil","mau","alta","Forte","mesmo","novo"],
     }
 }
 
@@ -48,15 +48,15 @@ PRODUCTION_RULES = {
     },
     "SPANISH":{
         "SENTENCE" : ["SUBJECTPREDICATE","SUBJECT"],
-        "SUBJECT" : ["PRONOUN"],
-        "PREDICATE" : ["VERBARTCILENOUNADJECTIVE","VERBARTCILEADJECTIVENOUN","VERBNOUN","VERBARTCILENOUN","VERB","CIRCUNSTANCE","MODIFIER"],
+        "SUBJECT" : ["ARTICLENOUN","PRONOUN"],
+        "PREDICATE" : ["VERBARTICLENOUNADJECTIVE","VERBARTICLEADJECTIVENOUN","VERBNOUN","VERBARTICLENOUN","VERBADJECTIVE","VERB","CIRCUNSTANCE","MODIFIER"],
         "MODIFIER" : ["CIRCUNSTANCE"],
         "CIRCUNSTANCE" : ["PREPOSITIONNOUN","CONJUNCTIONNOUN"]
     },
     "PORTUGUESE":{
         "SENTENCE" : ["SUBJECTPREDICATE","SUBJECT"],
-        "SUBJECT" : ["PRONOUN"],
-        "PREDICATE" : ["VERBNOUN","VERB","CIRCUNSTANCE","MODIFIER"],
+        "SUBJECT" : ["ARTICLENOUN","PRONOUN"],
+        "PREDICATE" : ["VERBARTICLENOUNADJECTIVE","VERBARTICLEADJECTIVENOUN","VERBNOUN","VERBARTICLENOUN","VERBADJECTIVE","VERB","CIRCUNSTANCE","MODIFIER"],
         "MODIFIER" : ["CIRCUNSTANCE"],
         "CIRCUNSTANCE" : ["PREPOSITIONNOUN","CONJUNCTIONNOUN"]
     }
